6b2605650688fb7f5af678c3709ffae26e94dff7
StormValen <valentin.g.l@gmail.com>
2018-04-25 01:46:08 +0200

    Enviando paquetes, solo INT no STRING

--- GameClient/Source.cpp
+++ GameClient/Source.cpp
@@ -1,9 +1,406 @@
-#pragma once
-#include <PlayerInfo.h>
+#include <stdlib.h>
+// #include <unistd.h>
+#include <fcntl.h>
+#include <signal.h>
+// #include <wait.h>
+#include <thread>
+#include <stdio.h>
+#include <iostream>
+#include <SFML/Graphics.hpp>
 #include <SFML\Network.hpp>
+
+#define MAX 100
+#define SIZE_TABLERO 64
+#define SIZE_FILA_TABLERO 25
+#define LADO_CASILLA 20
+#define RADIO_AVATAR 10.f
+#define OFFSET_AVATAR 1
+
+#define SIZE_TABLERO 64
+#define LADO_CASILLA 20
+#define RADIO_AVATAR 10.f
+#define OFFSET_AVATAR 1
+
+enum TipoProceso { RATON, GATO, PADRE };
+char tablero[SIZE_TABLERO];
+struct Player
+{
+	int ID;
+	float posX, posY;
+	std::string name;
+};
+struct Movment
+{
+	float movX, movY;
+	int IDMove;
+};
+/**
+* Si vale true --> nos permite marcar casilla con el mouse
+* Si vale false --> No podemos interactuar con el tablero y aparece un letrero de "esperando"
+*/
+bool tienesTurno = true;
+
+/**
+* Ahora mismo no tiene efecto, pero luego lo necesitarás para validar los movimientos
+* en función de si eres el gato o el ratón.
+*/
+TipoProceso quienSoy = TipoProceso::RATON;
+
+
+/**
+* Cuando el jugador clica en la pantalla, se nos da una coordenada del 0 al 500.
+* Esta función la transforma a una posición entre el 0 y el 24
+*/
+sf::Vector2f TransformaCoordenadaACasilla(int _x, int _y)
+{
+	float xCasilla = _x / LADO_CASILLA;
+	float yCasilla = _y / LADO_CASILLA;
+	sf::Vector2f casilla(xCasilla, yCasilla);
+	return casilla;
+}
+
+/**
+* Si guardamos las posiciones de las piezas con valores del 0 al 7,
+* esta función las transforma a posición de ventana (pixel), que va del 0 al 512
+*/
+sf::Vector2f BoardToWindows(sf::Vector2f _position)
+{
+	return sf::Vector2f(_position.x*LADO_CASILLA, _position.y*LADO_CASILLA);
+}
+
+sf::UdpSocket socket;
+Player player;
+std::map<int, Player>Players;
+int ID;
+Movment movActual;
+std::vector<Movment>listMovments;
+
+void old_resetMov(Movment* mov) {
+	mov->movX = 0;
+	mov->movY = 0;
+}
+void old_Connection(){
+	bool send = false;
+	sf::Packet packetLog;
+	std::string name;
+	std::cout << "Introduce tu nombre" << std::endl;
+	std::cin >> name;
+	packetLog << name;
+	sf::Clock c;
+	c.restart();
+	while (!send) {
+		if (c.getElapsedTime().asMilliseconds() >= 500) {
+			if (socket.send(packetLog, "localhost", 50000) != sf::Socket::Done) {
+				std::cout << "Error al enviar" << std::endl;
+			}
+			c.restart();
+			send = true;
+		}
+	}
+	packetLog.clear();
+
+	sf::IpAddress IP;
+	unsigned short port;
+	sf::Packet packR;
+	if (socket.receive(packR, IP, port) != sf::Socket::Done) {
+		std::cout << "Error al recivir";
+	}
+	std::string welcome = "";
+	int size = 0;
+	packR >> size;
+	for (int i = 0; i < size; i++) {
+		packR >> welcome >> player.name >> player.ID >> player.posX >> player.posY;
+		if (welcome == "CMD_WELCOME") {
+			//Players.(player);
+			Players.insert(std::pair<int, Player>(player.ID, player));
+			std::cout << Players[i].name << " ID:" << Players[i].ID << " POS: " << Players[i].posX << " " << Players[i].posY << std::endl;
+		}
+	}
+	packR.clear();
+}
+/**
+* Contiene el código SFML que captura el evento del clic del mouse y el código que pinta por pantalla
+*/
+void old_Gameplay()
+{
+	socket.setBlocking(false);
+	sf::Packet packS;
+	int idAux2 = 0;
+	sf::Vector2f casillaOrigen, casillaDestino;
+	bool casillaMarcada = false;
+	sf::Clock clockMov;
+	clockMov.restart();
+	sf::RenderWindow window(sf::VideoMode(500, 500), "El Gato y el Raton");
+	while (window.isOpen())
+	{
+		sf::Event event;
+		sf::Packet packMov;
+		sf::Packet pack;
+
+		std::string cmd;
+		sf::IpAddress _IP;
+		unsigned short _port;
+
+		if (socket.receive(pack, _IP, _port) != sf::Socket::Done) {
+			//std::cout << "Error al recivir";
+		}
+		pack >> cmd; 
+
+		if (cmd == "CMD_NEW_PLAYER") {
+			Player newPlayer;
+			pack >> newPlayer.name >> newPlayer.ID >> newPlayer.posX >> newPlayer.posY;
+			std::cout << " > " << cmd << " ID: " << newPlayer.ID << " POS: " << newPlayer.posX << newPlayer.posY << std::endl;
+			Players.insert(std::pair<int, Player>(newPlayer.ID, newPlayer));
+		//	Players.push_back(newPlayer);
+		}
+		else if (cmd == "CMD_PING") {
+			sf::Clock clock;
+			clock.restart();
+			packS << "CMD_ACK" << player.ID;
+			//if (clock.getElapsedTime().asMilliseconds() >= 100) {
+			if (socket.send(packS, "localhost", 50000) != sf::Socket::Done) {
+				std::cout << "Error al enviar" << std::endl;
+			}
+			clock.restart();
+		}
+		else if (cmd == "CMD_DESC") {
+			std::string a;
+			int idAux;
+			pack >> a >> idAux;
+			for (int i =0 ; i < Players.size(); i++) {
+				if (Players[i].ID == idAux) {
+					Players.erase(Players[i].ID);
+				}
+			}
+		}
+		//std::cout << cmd;
+		else if (cmd == "CMD_OK_MOVE") {
+			//int idAux2 = 0;
+			int idMoveAux = 0;
+			pack >> idAux2 >> idMoveAux;
+			std::cout << idAux2 << player.ID <<  std::endl;
+			for (std::map<int, Player>::iterator it = Players.begin(); it != Players.end(); ++it) {
+			//	std::cout << " X " << it->second.posX << " Y " << it->second.posY << std::endl;
+				if (it->first == idAux2) {
+					pack >> it->second.posX >> it->second.posY;
+					//std::cout << " X " << it->second.posX << " Y " << it->second.posY << std::endl;
+				}
+			}
+			pack.clear();
+		}
+		while (window.pollEvent(event))
+		{
+			switch (event.type)
+			{
+			case sf::Event::Closed:
+				window.close();
+				socket.unbind();
+				break;
+			case sf::Event::KeyPressed:
+				if (event.key.code == sf::Keyboard::Right) {
+					movActual.movX++;
+				}
+				if (event.key.code == sf::Keyboard::Left) {
+					movActual.movX--;
+				}
+				if (event.key.code == sf::Keyboard::Up) {
+					movActual.movY--;
+				}
+				if (event.key.code == sf::Keyboard::Down) {
+					movActual.movY++;
+				}
+
+				if (clockMov.getElapsedTime().asMilliseconds() > 200) {
+					movActual.IDMove++;
+					listMovments.push_back(movActual);
+					packMov << "CMD_MOV" << movActual.IDMove << player.ID << movActual.movX << movActual.movY;
+
+					if (socket.send(packMov, "localhost", 50000) != sf::Socket::Done) {
+						std::cout << "Error al enviar la posicion" << std::endl;
+					}
+					else {
+						//std::cout << "ID " << player.ID << " IDM " << movActual.IDMove << " X " << movActual.movX << " Y " << movActual.movY << std::endl;
+						old_resetMov(&movActual);
+						clockMov.restart();
+					}
+				}
+				break;
+			case sf::Event::MouseButtonPressed:
+				if (event.mouseButton.button == sf::Mouse::Left && tienesTurno)
+				{
+					int x = event.mouseButton.x;
+					int y = event.mouseButton.y;
+					if (!casillaMarcada)
+					{
+						casillaOrigen = TransformaCoordenadaACasilla(x, y);
+						casillaMarcada = true;
+						//TODO: Comprobar que la casilla marcada coincide con las posición del raton (si le toca al ratón)
+						//o con la posicion de alguna de las piezas del gato (si le toca al gato)
+
+					}
+					else
+					{
+						casillaDestino = TransformaCoordenadaACasilla(x, y);
+						if (casillaOrigen.x == casillaDestino.x && casillaOrigen.y == casillaDestino.y)
+						{
+							casillaMarcada = false;
+							//Si me vuelven a picar sobre la misma casilla, la desmarco
+						}
+						else
+						{
+							if (quienSoy == TipoProceso::RATON)
+							{
+								//TODO: Validar que el destino del ratón es correcto
+
+								//TODO: Si es correcto, modificar la posición del ratón y enviar las posiciones al padre
+
+							}
+							else if (quienSoy == TipoProceso::GATO)
+							{
+								//TODO: Validar que el destino del gato es correcto
+
+								//TODO: Si es correcto, modificar la posición de la pieza correspondiente del gato y enviar las posiciones al padre
+							}
+						}
+					}
+				}
+				break;
+			default:
+				break;
+
+			}
+		}
+
+		window.clear();
+
+		//A partir de aquí es para pintar por pantalla
+		//Este FOR es para el tablero
+		for (int i = 0; i<SIZE_FILA_TABLERO; i++)
+		{
+			for (int j = 0; j<SIZE_FILA_TABLERO; j++)
+			{
+				sf::RectangleShape rectBlanco(sf::Vector2f(LADO_CASILLA, LADO_CASILLA));
+				rectBlanco.setFillColor(sf::Color::White);
+				if (i % 2 == 0)
+				{
+					//Empieza por el blanco
+					if (j % 2 == 0)
+					{
+						rectBlanco.setPosition(sf::Vector2f(i*LADO_CASILLA, j*LADO_CASILLA));
+						window.draw(rectBlanco);
+					}
+				}
+				else
+				{
+					//Empieza por el negro
+					if (j % 2 == 1)
+					{
+						rectBlanco.setPosition(sf::Vector2f(i*LADO_CASILLA, j*LADO_CASILLA));
+						window.draw(rectBlanco);
+					}
+				}
+			}
+		}
+
+		//TODO: Para pintar el circulito del ratón
+		/*sf::CircleShape shapeRaton(RADIO_AVATAR);
+		shapeRaton.setFillColor(sf::Color::Blue);
+		sf::Vector2f posicionRaton(player.posX, player.posY);
+		posicionRaton = BoardToWindows(posicionRaton);
+		shapeRaton.setPosition(posicionRaton);
+		window.draw(shapeRaton);*/
+
+		for (std::map<int, Player>::iterator it = Players.begin(); it != Players.end(); ++it) {
+			sf::CircleShape shapeRaton(RADIO_AVATAR);
+			if (it->first == player.ID) {
+				shapeRaton.setFillColor(sf::Color::Blue);
+			}
+			else {
+				shapeRaton.setFillColor(sf::Color::Red);
+			}
+			sf::Vector2f positionGato1(it->second.posX, it->second.posY);
+			//positionGato1 = BoardToWindows(positionGato1);
+			shapeRaton.setPosition(positionGato1);
+
+			window.draw(shapeRaton);
+		}
+	
+	/*	//Pintamos los cuatro circulitos del gato
+		sf::CircleShape shapeGato(RADIO_AVATAR);
+		shapeGato.setFillColor(sf::Color::Red);
+
+		sf::Vector2f positionGato1(1.f, 0.f);
+		positionGato1 = BoardToWindows(positionGato1);
+		shapeGato.setPosition(positionGato1);
+
+		window.draw(shapeGato);
+
+		sf::Vector2f positionGato2(3.f, 0.f);
+		positionGato2 = BoardToWindows(positionGato2);
+		shapeGato.setPosition(positionGato2);
+
+		window.draw(shapeGato);
+
+		sf::Vector2f positionGato3(5.f, 0.f);
+		positionGato3 = BoardToWindows(positionGato3);
+		shapeGato.setPosition(positionGato3);
+
+		window.draw(shapeGato);*/
+		if (!tienesTurno)
+		{
+			//Si no tengo el turno, pinto un letrerito de "Esperando..."
+			sf::Font font;
+			std::string pathFont = "liberation_sans/LiberationSans-Regular.ttf";
+			if (!font.loadFromFile(pathFont))
+			{
+				std::cout << "No se pudo cargar la fuente" << std::endl;
+			}
+
+
+			sf::Text textEsperando("Esperando...", font);
+			textEsperando.setPosition(sf::Vector2f(180, 200));
+			textEsperando.setCharacterSize(30);
+			textEsperando.setStyle(sf::Text::Bold);
+			textEsperando.setFillColor(sf::Color::Green);
+			window.draw(textEsperando);
+		}
+		else
+		{
+			//Si tengo el turno y hay una casilla marcada, la marco con un recuadro amarillo.
+			if (casillaMarcada)
+			{
+				sf::RectangleShape rect(sf::Vector2f(LADO_CASILLA, LADO_CASILLA));
+				rect.setPosition(sf::Vector2f(casillaOrigen.x*LADO_CASILLA, casillaOrigen.y*LADO_CASILLA));
+				rect.setFillColor(sf::Color::Transparent);
+				rect.setOutlineThickness(5);
+				rect.setOutlineColor(sf::Color::Yellow);
+				window.draw(rect);
+			}
+		}
+		window.display();
+	}
+}
+
+
+
+
+
+void Connection() {
+	sf::Packet packetHELLOE;
+
+	sf::Packet packetLog;
+	std::string name;
+	std::cout << "Introduce tu nombre" << std::endl;
+	std::cin >> name;
+	packetLog << name;
+	if (socket.send(packetLog, "localhost", 50000) != sf::Socket::Done) {
+		std::cout << "ERROR: An error has ocurred when sending a packet" << std::endl;
+	}
+	packetHELLOE.clear();
+}
 
 int main()
 {
-	PlayerInfo playerInfo;
+	old_Connection();
 	return 0;
 }
--- GameServer/Source.cpp
+++ GameServer/Source.cpp
@@ -1,13 +1,220 @@
-#pragma once
-#include <iostream>
-#include <SFML\Graphics.hpp>
 #include <SFML\Network.hpp>
-#include <PlayerInfo.h>
+#include <iostream>
+#include <vector>
+#include <list>
+#include <thread>
+#include <mutex>
+#include <cstring>
+
+#define MAX_PLAYERS 4
+sf::UdpSocket socket;
+
+struct Movment
+{
+	float movX, movY;
+	int IDMove;
+};
+struct Player
+{
+	sf::IpAddress IP;
+	unsigned short port;
+	float posX, posY;
+	std::string name;
+	sf::Clock timePing;
+	Movment movment;
+};
+
+int ID;
+std::map<int, Player> Players;
+float maxY = 480;
+float minY = 1.f;
+float maxX = 480.f;
+float minX = 1.f;
+//StateModes --> chat_mode - countdown_mode - bet_money_mode - bet_number_mode - simulate_game_mode - bet_processor_mode
+
+void old_Recorrer() {
+	for (std::map<int, Player>::iterator it = Players.begin(); it != Players.end(); ++it) {
+		for (std::map<int, Player>::iterator it2 = Players.begin(); it2 != Players.end(); ++it2) {
+			sf::Packet pack;
+			pack << it2->first << it2->second.posX << it2->second.posY;
+			if (socket.send(pack, it->second.IP, it->second.port) != sf::Socket::Done) {
+				std::cout << "error";
+			}
+		}
+	}
+
+}
+
+void old_Connection() {
+	srand(time(NULL));
+	sf::Packet packetLog;
+	sf::Packet newPlayerPack;
+
+	socket.bind(50000);
+	Player player;
+	for (int i = 0; i < MAX_PLAYERS; i++) {
+		sf::IpAddress IP;
+		unsigned short port;
+		if (socket.receive(packetLog, IP, port) != sf::Socket::Done) {
+			std::cout << "Error al recivir" << std::endl;
+		}
+		packetLog >> player.name;
+		player.IP = IP;
+		player.port = port;
+		player.posX = rand() % 499;
+		player.posY = rand() % 499;
+
+		for (std::map<int, Player>::iterator it = Players.begin(); it != Players.end(); ++it) {
+			std::string cmd = "CMD_NEW_PLAYER";
+			newPlayerPack << cmd << player.name  << i << player.posX << player.posY;
+			if (socket.send(newPlayerPack, it->second.IP, it->second.port) != sf::Socket::Done) {
+				std::cout << "Error al enviar nueva conexion" << std::endl;
+			}
+			newPlayerPack.clear();
+		}
+
+		Players.insert(std::pair<int, Player>(i, player));
+		packetLog.clear();
+		ID = i;
+		packetLog << (int)Players.size();
+
+		for (std::map<int, Player>::iterator it = Players.begin(); it != Players.end(); ++it) {
+			std::string cmd = "CMD_WELCOME";
+			packetLog << cmd << it->second.name << it->first << it->second.posX << it->second.posY;
+			std::cout << "Se ha conectado : " << it->second.name << cmd << it->first << it->second.posX << it->second.posY << std::endl;
+		}
+		if (socket.send(packetLog, IP, port) != sf::Socket::Done) {
+			std::cout << "error";
+		}
+		packetLog.clear();
+	}
+	
+}
+
+void old_sendAllPlayers(std::string msg, int id) {
+	sf::Packet packDes;
+	for (std::map<int, Player>::iterator it = Players.begin(); it != Players.end(); ++it) {
+		std::string cmd = "CMD_DESC";
+		packDes << cmd << msg << id;
+		if (socket.send(packDes, it->second.IP, it->second.port) != sf::Socket::Done) {
+			std::cout << "Error al enviar nueva conexion" << std::endl;
+		}
+		packDes.clear();
+	}
+}
+
+void old_Game() {
+	for (std::map<int, Player>::iterator it = Players.begin(); it != Players.end(); ++it) {
+		it->second.timePing.restart();
+	}
+	socket.setBlocking(false);
+	sf::IpAddress IP;
+	unsigned short port;
+	bool send = false;
+	sf::Packet packR, packPingS;
+	sf::Clock clockP;
+	clockP.restart();
+	std::string ping;
+	int id;
+	std::string cmd;
+	while (true) {
+		if (clockP.getElapsedTime().asMilliseconds() > 1000) {
+			for (std::map<int, Player>::iterator it = Players.begin(); it != Players.end(); ++it) {
+				ping = "CMD_PING";
+				packPingS << ping;
+				if (socket.send(packPingS, it->second.IP, it->second.port) != sf::Socket::Done) {
+					std::cout << "Error al enviar el ping" << std::endl;
+				}
+				else {
+					clockP.restart();
+					packPingS.clear();
+				}
+			}
+		}
+		for (std::map<int, Player>::iterator it = Players.begin(); it != Players.end(); ++it) {
+			if (it->second.timePing.getElapsedTime().asSeconds() >= 5) {
+				old_sendAllPlayers("Desconectado con la ID: ", it->first);
+				std::cout << "Desconexion con la ID: " << it->first << std::endl;
+				Players.erase(it->first);
+			}
+		}
+		if (socket.receive(packR, IP, port) != sf::Socket::Done) {
+		}
+		else {
+			packR >> cmd;
+			if (cmd == "CMD_ACK") {
+				packR >> id;
+				//std::cout << id << ping;
+				Players.find(id)->second.timePing.restart();
+			}
+			if (cmd == "CMD_MOV") {
+				int idMove, id;
+				float deltaX, deltaY;
+				packR >> idMove >> id >> deltaX >> deltaY;
+				std::string a = "CMD_OK_MOVE";
+				sf::Packet packM;
+				packM << a;
+				for (std::map<int, Player>::iterator it = Players.begin(); it != Players.end(); ++it) {
+					for (std::map<int, Player>::iterator it2 = Players.begin(); it2 != Players.end(); ++it2) {
+						if (it2->first == id) {
+							it2->second.movment.IDMove = idMove;
+							if ((it2->second.posX += deltaX) > maxX || (it2->second.posX += deltaX) < minX || (it2->second.posY += deltaY) > maxY || (it2->second.posY += deltaY) < minY) {
+								it2->second.movment.movX = 0;
+								it2->second.movment.movY = 0;
+							}
+							else {
+								it2->second.movment.movX = deltaX;
+								it2->second.movment.movY = deltaY;
+								it2->second.posX += it2->second.movment.movX;
+								it2->second.posY += it2->second.movment.movY;
+								packM << it2->first << it2->second.movment.IDMove << it2->second.posX << it2->second.posY;
+								//std::cout << " ID" << it2->first << " IDM " << it2->second.movment.IDMove << " X " << deltaX << " Y " << deltaY << " posX " << it2->second.posX << " posY " << it2->second.posY;
+							}
+						}
+					}
+					if (socket.send(packM, it->second.IP, it->second.port) != sf::Socket::Done) {
+						std::cout << "Error al enviar mov" << std::endl;
+					}
+					//std::cout << it->second.name << it->second.IP << it->second.port << std::endl;
+				}
+				packM.clear();
+			}
+		}
+		packR.clear();
+	}
+}
+
+
+
+
+
+
+void Connection() {
+	socket.bind(50000);
+
+	sf::IpAddress player_Ip;
+	unsigned short player_Port;
+
+	sf::Packet player_packetRec;
+	if (socket.receive(player_packetRec, player_Ip, player_Port) != sf::Socket::Done) {
+		std::cout << "ERROR: An error has ocurred when receiveing a packet" << std::endl;
+	}
+
+	std::string cmd;
+	int num;
+	player_packetRec >> cmd;
+
+	std::cout << "PLAYER IP: " << player_Ip << " | " << "PLAYER PORT: " << player_Port << " | " << "PLAYER PACKET: " << cmd << std::endl;
+	player_packetRec.clear();
+}
+
 
 int main()
 {
-	
-	PlayerInfo playerInfo;
+	// 1. Establecimiento de connexión
+	Connection();
 	system("pause");
+	// 2. Game Loop
+	// 3. Gestion de desconnexiones
 	return 0;
 }
