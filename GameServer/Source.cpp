969ff95b4c3888d11517aaccaa87d81ec5bcbfbd
StormValen <valentin.g.l@gmail.com>
2018-04-25 00:38:22 +0200

    First Commit

---
+++ GameServer/Source.cpp
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+#include <SFML\Graphics.hpp>
+#include <SFML\Network.hpp>
+#include <PlayerInfo.h>
+
+int main()
+{
+	
+	PlayerInfo playerInfo;
+	system("pause");
+	return 0;
+}
